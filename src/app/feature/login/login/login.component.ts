53df8c54f02f0e8f5d80fd5a16ba1ad13100c17e
leinerdev <leinerbarrios99.ljbm@gmail.com>
2022-12-20 17:58:18 -0500

    Inicia sesión, debe aparecer el error por correo y clave vacío

--- src/app/feature/login/login/login.component.ts
+++ src/app/feature/login/login/login.component.ts
@@ -41,6 +41,7 @@ export class LoginComponent implements OnInit{
       localStorage.setItem('token', loginResponse.token);
       this.redirectUsers();
     } catch (errorJson) {
+      this.loginForm.markAllAsTouched();
       const { error } = errorJson;
       this.error = error.error;
     }
