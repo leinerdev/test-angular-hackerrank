58354b1b0d14b8fbc1f75718822744b81c75a8ff
leinerdev <leinerbarrios99.ljbm@gmail.com>
2022-12-21 11:12:51 -0500

    Test opcionales: Test unitario al UserService

--- src/app/feature/users/create-user/shared/services/users/users.service.spec.ts
+++ src/app/feature/users/create-user/shared/services/users/users.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { UsersService } from './users.service';
@@ -6,8 +7,17 @@ describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+      ]
+    });
     service = TestBed.inject(UsersService);
+  });
+
+  // ========== Tests opcionales ========== //
+  it('should be creaetd', () => {
+    expect(service).toBeTruthy();
   });
 
 });
